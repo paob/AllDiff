82d9c6cec249333cae29115289c9aac1a42aedc6
Paul Beaujean <paul.beaujean@ensiie.fr>
2014-05-27 21:49:09 +0200

    add a probably not correct alternating path function and make some stylistic changes

--- bipartite.hs
+++ bipartite.hs
@@ -38,21 +38,34 @@ a ^ b = filter (\x -> x `elem` b) a
 -- symetric difference
 a <+> b = (a |-- b) ++ (a |-- b)
 
-makeEdge :: Node -> Node -> Maybe Edge
-makeEdge (Nx x) (Ny y) = Just (x, y)
-makeEdge (Ny y) (Nx x) = Just (x, y)
-makeEdge _ _           = Nothing
+(<-->) :: Node -> Node -> Maybe Edge
+(Nx x) <--> (Ny y) = Just (x, y)
+(Ny y) <--> (Nx x) = Just (x, y)
+_ <--> _           = Nothing
 
-inMatching :: Node -> Matching -> Bool
-inMatching (Nx x) (Matching es) = x `elem` (fst . unzip $ es)
-inMatching (Ny y) (Matching es) = y `elem` (snd . unzip $ es)
+isSaturatedIn :: Node -> Matching -> Bool
+isSaturatedIn (Nx x) (Matching es) = x `elem` (fst . unzip $ es)
+isSaturatedIn (Ny y) (Matching es) = y `elem` (snd . unzip $ es)
 
 findAvailableEdge :: Graph -> Node -> Matching -> Matching
 findAvailableEdge g v m@(Matching es) = case w of
-    Just n  -> Matching $ (fromJust $ makeEdge v n) : es
+    Just n  -> let Just e = v <--> n in Matching $ e:es
     Nothing -> m
-    where w = find (\n -> not $ n `inMatching` m) (neighbours g v)
+    where w = find (\n -> not $ n `isSaturatedIn` m) (neighbours g v)
 
 matching :: Graph -> Matching
 matching g = matching' g (Matching [])
     where matching' g@(Graph xs _ _) m = foldr (findAvailableEdge g) m (Nx <$> xs)
+
+augmentingPath :: Graph -> Matching -> Node -> [Edge]
+augmentingPath g m root = augmentingPath' g m root []
+    where augmentingPath' g m root visited = case (neighbours g root) |-- visited of
+            []   -> []
+            v:vs ->
+                if (v `isSaturatedIn` m) then
+                    case matched m v of
+                        Just n  -> e1 : e2 : (augmentingPath' g m n (v:visited))
+                                   where Just e1 = n <--> v
+                                         Just e2 = v <--> root
+                        Nothing -> augmentingPath' g m root (v:visited)
+                else [e] where Just e = v <--> root
