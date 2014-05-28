464af50b79b765c5b4337a6617836ddf2504804a
Paul Beaujean <paul.beaujean@ensiie.fr>
2014-05-28 16:22:19 +0200

    rewrite of the maximal matching algorithm, use Either, remove the probably wrong augmenting path function

--- bipartite.hs
+++ bipartite.hs
@@ -7,7 +7,7 @@ import Control.Applicative ((<$>))
 data NodeX = X Int deriving (Show, Eq)
 data NodeY = Y Int deriving (Show, Eq)
 
-data Node = Nx NodeX | Ny NodeY deriving (Show, Eq)
+type Node = Either NodeX NodeY
 
 type Edge = (NodeX, NodeY)
 
@@ -15,13 +15,15 @@ data Graph = Graph [NodeX] [NodeY] [Edge] deriving (Show, Eq)
 
 data Matching = Matching [Edge]
 
+-- Higher-order function implementing set irrelevance: allow application of
+-- functions on nodes regardless of which set the node belongs to.
 graph :: Functor f =>
      ((Edge -> Bool) -> [Edge] -> f Edge)
      -> Node 
      -> [Edge]
      -> f Node
-graph f (Nx x) es = (Ny . snd) <$> f (\(u, _) -> u == x) es
-graph f (Ny y) es = (Nx . fst) <$> f (\(_, v) -> v == y) es
+graph f (Left  x) es = (Right . snd) <$> f (\(u, _) -> u == x) es
+graph f (Right y) es = (Left  . fst) <$> f (\(_, v) -> v == y) es
 
 neighbours :: Graph -> Node -> [Node]
 neighbours (Graph _ _ es) n = graph filter n es
@@ -39,33 +41,21 @@ a ^ b = filter (\x -> x `elem` b) a
 a <+> b = (a |-- b) ++ (a |-- b)
 
 (<-->) :: Node -> Node -> Maybe Edge
-(Nx x) <--> (Ny y) = Just (x, y)
-(Ny y) <--> (Nx x) = Just (x, y)
+(Left  x) <--> (Right y) = Just (x, y)
+(Right y) <--> (Left  x) = Just (x, y)
 _ <--> _           = Nothing
 
 isSaturatedIn :: Node -> Matching -> Bool
-isSaturatedIn (Nx x) (Matching es) = x `elem` (fst . unzip $ es)
-isSaturatedIn (Ny y) (Matching es) = y `elem` (snd . unzip $ es)
+isSaturatedIn (Left  x) (Matching es) = x `elem` (fst . unzip $ es)
+isSaturatedIn (Right y) (Matching es) = y `elem` (snd . unzip $ es)
 
-findAvailableEdge :: Graph -> Node -> Matching -> Matching
-findAvailableEdge g v m@(Matching es) = case w of
-    Just n  -> let Just e = v <--> n in Matching $ e:es
-    Nothing -> m
-    where w = find (\n -> not $ n `isSaturatedIn` m) (neighbours g v)
+addIndependentEdge :: Edge -> Matching -> Matching
+addIndependentEdge e@(u,v) m@(Matching es) 
+    | (Left u) `isSaturatedIn` m || (Right v) `isSaturatedIn` m = m
+    | otherwise = Matching (e:es)
 
-matching :: Graph -> Matching
-matching g = matching' g (Matching [])
-    where matching' g@(Graph xs _ _) m = foldr (findAvailableEdge g) m (Nx <$> xs)
-
-augmentingPath :: Graph -> Matching -> Node -> [Edge]
-augmentingPath g m root = augmentingPath' g m root []
-    where augmentingPath' g m root visited = case (neighbours g root) |-- visited of
-            []   -> []
-            v:vs ->
-                if (v `isSaturatedIn` m) then
-                    case matched m v of
-                        Just n  -> e1 : e2 : (augmentingPath' g m n (v:visited))
-                                   where Just e1 = n <--> v
-                                         Just e2 = v <--> root
-                        Nothing -> augmentingPath' g m root (v:visited)
-                else [e] where Just e = v <--> root
+-- A maximal matching is a matching M of a graph G with the property that if
+-- any edge not in M is added to M, it is no longer a matching
+maximalMatching :: Graph -> Matching
+maximalMatching g = maximalMatching' g (Matching [])
+    where maximalMatching' (Graph _ _ es) m = foldr addIndependentEdge m es
